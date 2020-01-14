92a61b96bbb545fb7a71e68debdb1e30541d3143
oupo <oupo.nejiki@gmail.com>
2020-01-14 22:49:25 +0900

    4色QR出力の布石

--- ndswide/source/main.cpp
+++ ndswide/source/main.cpp
@@ -51,7 +51,6 @@ void dumpQR(u16 *videoMemoryMain, int blockid, uint8_t *buf, int len)
 	vec[2] = (blockid >> 16) & 0xff;
 	vec[3] = (blockid >> 24) & 0xff;
 	std::copy(buf, buf + len, vec.begin() + 4);
-	const QrCode::Ecc errCorLvl = QrCode::Ecc::MEDIUM; // Error correction level
 	std::vector<QrSegment> segs{QrSegment::makeEci(27), QrSegment::makeBytes(vec)};
 	int mask = 0; // fix mask for speed
 	const QrCode qr = QrCode::encodeSegmentsWide(segs, mask);
@@ -71,7 +70,51 @@ void dumpQR(u16 *videoMemoryMain, int blockid, uint8_t *buf, int len)
 	fillRect(videoMemoryMain, 2, 192 - 6, 4, 4, ARGB16(1, 28, 11, 21));
 }
 
+QrCode makeQR(int blockid, uint8_t *buf, int len) {
+	std::vector<uint8_t> vec(4 + len);
+	vec[0] = blockid & 0xff;
+	vec[1] = (blockid >> 8) & 0xff;
+	vec[2] = (blockid >> 16) & 0xff;
+	vec[3] = (blockid >> 24) & 0xff;
+	std::copy(buf, buf + len, vec.begin() + 4);
+	std::vector<QrSegment> segs{QrSegment::makeEci(27), QrSegment::makeBytes(vec)};
+	int mask = 0; // fix mask for speed
+	return QrCode::encodeSegmentsWide(segs, mask);
+}
+
+void dumpQR2(u16 *videoMemoryMain, int blockid1, int blockid2, uint8_t *buf1, uint8_t *buf2, int len)
+{
+	QrCode qr1 = makeQR(blockid1, buf1, len);
+	QrCode qr2 = makeQR(blockid2, buf2, len);
+	int w = qr1.getWidth(), h = qr1.getHeight();
+	for (int y = -1; y < h + 1; y++)
+	{
+		for (int x = -1; x < w + 1; x++)
+		{
+			int c1 = qr1.getModule(x, y) ? 0 : 31;
+			int c2 = qr2.getModule(x, y) ? 0 : 31;
+			int c3 = c1 & c2;
+			fillRect(videoMemoryMain, 2 * x + 2, 2 * y + 2, 2, 2, ARGB16(1, c1, c2, c3));
+		}
+	}
+	fillRect(videoMemoryMain, 2, 2, 4, 4, ARGB16(1, 8, 16, 29));
+	fillRect(videoMemoryMain, 256 - 6, 2, 4, 4, ARGB16(1, 3, 26, 10));
+	fillRect(videoMemoryMain, 256 - 6, 192 - 6, 4, 4, ARGB16(1, 28, 20, 7));
+	fillRect(videoMemoryMain, 2, 192 - 6, 4, 4, ARGB16(1, 28, 11, 21));
+}
+
 const int BLOCK_SIZE = 0x1c0;
+
+void writeRandomData(std::vector<uint8_t> &data, uint32_t seed) {
+	data.resize(BLOCK_SIZE);
+	for (int i = 0; i < BLOCK_SIZE / 4; i ++) {
+		seed = seed * 0x41c64e6d + 0x6073;
+		data[i*4+0] = seed & 0xff;
+		data[i*4+1] = (seed >> 8) & 0xff;
+		data[i*4+2] = (seed >> 16) & 0xff;
+		data[i*4+3] = (seed >> 24) & 0xff;
+	}
+}
 
 void dump(void)
 {
@@ -81,16 +124,12 @@ void dump(void)
 	u16 *videoMemoryMain = bgGetGfxPtr(bgMain);
 	for (int i = 0; i < 256 * 256; i++)
 		videoMemoryMain[i] = ARGB16(1, 31, 31, 31);
-	std::vector<uint8_t> testdata(BLOCK_SIZE);
-	uint32_t seed = 0xdeadbeef;
-	for (int i = 0; i < BLOCK_SIZE / 4; i ++) {
-		seed = seed * 0x41c64e6d + 0x6073;
-		testdata[i*4+0] = seed & 0xff;
-		testdata[i*4+1] = (seed >> 8) & 0xff;
-		testdata[i*4+2] = (seed >> 16) & 0xff;
-		testdata[i*4+3] = (seed >> 24) & 0xff;
-	}
+	std::vector<uint8_t> testdata;
+	writeRandomData(testdata, 0xdeadbeef);
 	dumpQR(videoMemoryMain, 0, &testdata[0], BLOCK_SIZE);
+/*	std::vector<uint8_t> testdata2;
+	writeRandomData(testdata2, 0xcafecafe);
+	dumpQR2(videoMemoryMain, 0, 1, &testdata[0], &testdata2[0], BLOCK_SIZE); */
 	printf("drawed test data. push A\n");
 	waitKey();
 	char name[13] = {};
